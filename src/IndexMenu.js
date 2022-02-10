95383bb0db8699c1aec9dd29fb8409cba8e151cf
ohiro18 <hironobu.ohara@g.softbank.co.jp>
2022-02-10 22:24:42 +0900

    update svg provider

--- src/IndexMenu.js
+++ src/IndexMenu.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './components/styles.css';
 // eslint-disable-next-line import/no-unresolved
+
 import IndexImg from './components/images/index_background-image.svg';
+
 
 const IndexMenu = () => {
   return (
     <>
-      <div className="indexImgSection">
-        <img src={IndexImg} alt='Index_backgroundimage'/>
+      <div className="indexImgSection">      
+        <img className="IndexImg" src={IndexImg} alt='Index_backgroundimage'/>
       </div>
     </>
   );
